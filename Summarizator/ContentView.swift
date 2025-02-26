e7ea9d0fc334d76bae31af919c7a37abc9e9acee
Hubert <h.wojtowicz44@gmail.com>
2025-02-26 12:04:41 +0100

    Initial Commit

---
+++ Summarizator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Summarizator
+//
+//  Created by Hubert Wojtowicz on 26/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Summarizator/SummarizatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummarizatorApp.swift
+//  Summarizator
+//
+//  Created by Hubert Wojtowicz on 26/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SummarizatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
